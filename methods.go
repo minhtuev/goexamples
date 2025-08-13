8b06bcfe665ed00cdbe6379cd844991340bd61ce
Minh Vo <minhvo@Minhs-MacBook-Pro.local>
2025-08-13 16:51:07 -0700

    Updated methods and interfaces

--- methods.go
+++ methods.go
@@ -6,12 +6,28 @@ type rect struct {
     width, height int
 }
 
+// Method with pointer receiver
 func (r *rect) area() int {
     return r.width * r.height
 }
 
+// Method with value receiver
 func (r rect) perim() int {
     return 2*r.width + 2*r.height
+}
+
+// Method with pointer receiver that modifies the struct
+func (r *rect) scale(factor int) {
+    r.width *= factor
+    r.height *= factor
+}
+
+// Method with value receiver that returns a copy
+func (r rect) double() rect {
+    return rect{
+        width:  r.width * 2,
+        height: r.height * 2,
+    }
 }
 
 func main() {
@@ -23,4 +39,13 @@ func main() {
     rp := &r
     fmt.Println("area: ", rp.area())
     fmt.Println("perim:", rp.perim())
+
+    // Using the scale method (pointer receiver modifies the original)
+    r.scale(2)
+    fmt.Println("scaled area:", r.area(), "scaled perim:", r.perim())
+
+    // Using the double method (value receiver returns a new rect)
+    r2 := r.double()
+    fmt.Println("original after double():", r.area(), r.perim())
+    fmt.Println("new doubled rect:", r2.area(), r2.perim())
 }
